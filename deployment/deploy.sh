5abbb26696046608074b2039e78c982961851c86
Andy Wong <yiuc88@gmail.com>
2020-03-25 18:20:30 +0800

    stage file

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,6 +3,6 @@
 #Usage: CONTAINER_VERSION=docker_container_version [create|update]
 
 # register task-definition
-sed <td-nginx.template -e "s,@VERSION@,$CONTAINER_VERSION,">TASKDEF.json
+sed <deployment/td-nginx.template -e "s,@VERSION@,$CONTAINER_VERSION,">TASKDEF.json
 aws ecs register-task-definition --cli-input-json file://TASKDEF.json > REGISTERED_TASKDEF.json
 TASKDEFINITION_ARN=$( < REGISTERED_TASKDEF.json jq .taskDefinition.taskDefinitionArn )
