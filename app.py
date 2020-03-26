f74396649cbef6a734289aaabeef18b907174b63
Andy Wong <yiuc88@gmail.com>
2020-03-26 11:14:03 +0800

    add the page

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import time, json, io, datetime
-from quart import Quart
+from flask import Flask, request, render_template
 
-app = Quart(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def get_index():
     count = 0
     return 'v1\n'.format(count)
+
+@app.route('/ad')
+def get_ad():
+    return 'ad\n'.format()
+
+@app.route("/ad-event", methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        return 'Hello ' + request.form.get('adname')    
+    return render_template('post_ad.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
