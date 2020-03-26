95dafecab2b9af5c5db502dbe9f61161d80da567
Andy Wong <yiuc88@gmail.com>
2020-03-26 09:54:22 +0800

    add deployment in code build

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,8 +1,21 @@
 #!/bin/sh -e
-
-#Usage: CONTAINER_VERSION=docker_container_version [create|update]
+set -e
 
 # register task-definition
 sed <deployment/td-nginx.template -e "s,@VERSION@,${1},">TASKDEF.json
 aws ecs register-task-definition --cli-input-json file://TASKDEF.json > REGISTERED_TASKDEF.json
 TASKDEFINITION_ARN=$( < REGISTERED_TASKDEF.json jq .taskDefinition.taskDefinitionArn )
+
+# create or update service
+app_spec_content_string=$(jq -nc \
+  --arg container_name "web" \
+  --arg container_port "5000" \
+  --arg task_definition_arn "$TASKDEFINITION_ARN" \
+  '{version: 1, Resources: [{TargetService: {Type: "AWS::ECS::Service", Properties: {TaskDefinition: $task_definition_arn, LoadBalancerInfo: {ContainerName: $container_name, ContainerPort: $container_port}}}}]}')
+app_spec_content_sha256=$(echo -n "$app_spec_content_string" | shasum -a 256 | sed 's/ .*$//')
+revision="revisionType=AppSpecContent,appSpecContent={content='$app_spec_content_string',sha256=$app_spec_content_sha256}"
+
+aws --profile example deploy create-deployment \
+  --application-name="kaios-deploy" \
+  --deployment-group-name="kaios-deploy" \
+  --revision="$revision"
