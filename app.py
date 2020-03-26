15f3e2ef03f6bb426ff0e9ea2e95c28e1de054aa
Andy Wong <yiuc88@gmail.com>
2020-03-26 09:36:45 +0800

    change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Quart(__name__)
 @app.route('/')
 def get_index():
     count = 0
-    return 'Yo! 你是第 {} 次瀏覽\n'.format(count)
+    return 'v1\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
